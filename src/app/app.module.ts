90a18210eb4842f05272d541dc42a35730ab6903
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-06 15:32:11 -0500

    Agregamos componente ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgstyleComponent } from './componentes/ngstyle/ngstyle.component';
+import { NgclassComponent } from './componentes/ngclass/ngclass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NgstyleComponent,
+    NgclassComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/ngclass/ngclass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngclass',
+  templateUrl: './ngclass.component.html',
+  styles: []
+})
+export class NgclassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
