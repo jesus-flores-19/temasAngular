59fa9c3e6ee0b59d168afa4c39817fe75b65cebe
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-06 14:23:17 -0500

    Agregamos componente ngstyle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgstyleComponent } from './componentes/ngstyle/ngstyle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgstyleComponent
   ],
   imports: [
     BrowserModule,
