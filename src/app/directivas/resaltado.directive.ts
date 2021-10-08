2e35eca4ec0044851dc0e121fdf4caf7c8a1e945
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-07 21:43:05 -0500

    backgroundColor por directiva

--- src/app/directivas/resaltado.directive.ts
+++ src/app/directivas/resaltado.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor() { }
+  constructor(public element: ElementRef) { 
+    console.log("Directiva activada")
+    this.element.nativeElement.style.backgroundColor = "yellow"
+  }
+
+
 
 }
