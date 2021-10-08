b4ea2cea18443fe0fc851e03ebc57de9be7c9704
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-08 00:30:22 -0500

    Creamos enrutamiento de rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './componentes/home/home.component';
+import { UsuariosDetalleComponent } from './componentes/usuarios/usuarios-detalle.component';
+import { UsuariosEditarComponent } from './componentes/usuarios/usuarios-editar.component';
+import { UsuariosNuevosComponent } from './componentes/usuarios/usuarios-nuevos.component';
 import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
 
 const routes: Routes = [
   {path: "home", component:HomeComponent},
-  {path: "usuarios", component:UsuariosComponent},
+  {
+    path: "usuarios/:id", 
+    component:UsuariosComponent,
+    children:[
+      {path: "nuevo", component:UsuariosNuevosComponent},
+      {path: "editar", component:UsuariosEditarComponent},
+      {path: "detalle", component: UsuariosDetalleComponent}
+    ]
+  },
   {path: "**", pathMatch: "full", redirectTo: "home"}
 
 ];
